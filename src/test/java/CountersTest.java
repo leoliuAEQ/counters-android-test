8b901b259728f7f8d6164f41b1d4c1881b925931
Denis Pitul <denis.pitul@gmail.com>
2017-07-12 18:22:21 +0000

    - removing comments

--- src/test/java/CountersTest.java
+++ src/test/java/CountersTest.java
@@ -17,18 +17,6 @@ import static org.testng.Assert.assertTrue;
 public class CountersTest {
     protected AppiumDriver driver;
 
-//    @BeforeMethod(alwaysRun = true)
-//    public void setUp() throws MalformedURLException {
-//        DesiredCapabilities desiredCaps = new DesiredCapabilities();
-//        desiredCaps.setCapability("platformName", "ios");
-//        desiredCaps.setCapability("platformVersion", "10.3");
-//        desiredCaps.setCapability("appiumVersion", "1.6.5");
-//        desiredCaps.setCapability("deviceName", "iPhone 7");
-//        //this is not the current version of this file. This is not a git repo?
-//        desiredCaps.setCapability("app", Paths.get("Counters.app").toAbsolutePath().toString());
-//        driver = new AppiumDriver(new URL("http://10.1.1.184:4444/wd/hub"), desiredCaps);
-//    }
-
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws MalformedURLException {
         DesiredCapabilities desiredCaps = new DesiredCapabilities();
