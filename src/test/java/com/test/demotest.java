a657d39902d9268061abc276523f3fa8f55b4d96
NAREN YASA <narenyasa@NARENs-MacBook-Pro.local>
2017-07-28 12:21:10 -0700

    test

--- src/test/java/com/test/demotest.java
+++ src/test/java/com/test/demotest.java
@@ -4,6 +4,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -54,6 +58,7 @@ appiumService.start();
 		driver = new AndroidDriver(new URL(appiumServiceUrl), capabilities);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	}
+
 
 
 @AfterMethod(alwaysRun = true)
